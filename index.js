a8293152642b3fb72177be29bdc44a68ad7a8e01
zbx <zbx@localhost.localdomain>
2023-05-18 10:48:29 +0200

    Added saving to zabbix TODO documentation, error handling, make script run automatically

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 const fetch = require("node-fetch");
 
-const filterData = (hostData) => {
+const filteInventoryrData = (inventoryData) => {
   let filtredData = [];
-  hostData.forEach((data) => {
+  inventoryData.forEach((data) => {
     if (data.inventory.location == "") {
       return;
     }
     if (
-      data.inventory.location_lan != "" &&
+      data.inventory.location_lat != "" &&
       data.inventory.location_lon != ""
     ) {
       return;
@@ -18,7 +18,7 @@ const filterData = (hostData) => {
   return filtredData;
 };
 
-const getDataFromZabbix = async () => {
+const getInventoryData = async () => {
   let zabbixData;
   await fetch("http://localhost:8080/api_jsonrpc.php", {
     method: "POST",
@@ -45,9 +45,39 @@ const getDataFromZabbix = async () => {
   return zabbixData;
 };
 
-const convertAdressToLonLat = async (adress) => {
+const updateHostInventory = async (modifiedInventoryData) => {
+  await fetch("http://localhost:8080/api_jsonrpc.php", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      jsonrpc: "2.0",
+      method: "host.update",
+      params: {
+        hostid: modifiedInventoryData.hostid,
+        inventory: {
+          location: modifiedInventoryData.inventory.location,
+          location_lat: modifiedInventoryData.inventory.location_lat,
+          location_lon: modifiedInventoryData.inventory.location_lon,
+        },
+      },
+      auth: process.env.API_KEY,
+      id: 1,
+    }),
+  })
+    .then((data) => {
+      return data.json();
+    })
+    .then((data) => {
+      zabbixData = data;
+      console.log(data);
+    });
+};
+
+const convertAdressToGeolocation = async (location) => {
   let geoData;
-  await fetch(`https://geocode.maps.co/search?q={${adress}}`, {
+  await fetch(`https://geocode.maps.co/search?q={${location}}`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -67,33 +97,35 @@ async function sleep(ms) {
   });
 }
 
-const modifyHostData = async (hostData) => {
+const modifyInventoryData = async (inventoryData) => {
   const geoloactionDataToUpdate = [];
   let modifiedHost;
   let geoData;
-  for (const host in hostData) {
+  for (const inventory in inventoryData) {
     await sleep(600);
-    modifiedHost = hostData[host];
-    geoData = await convertAdressToLonLat(modifiedHost.inventory.location);
-    modifiedHost.inventory.location_lon = geoData.length>0
-      ? geoData[0].lon
-      : "";
-    modifiedHost.inventory.location_lat = geoData.length>0
-      ? geoData[0].lat
-      : "";
+    modifiedHost = inventoryData[inventory];
+    geoData = await convertAdressToGeolocation(modifiedHost.inventory.location);
+    modifiedHost.inventory.location_lon =
+      geoData.length > 0 ? geoData[0].lon.substring(0,16) : "";
+    modifiedHost.inventory.location_lat =
+      geoData.length > 0 ? geoData[0].lat.substring(0,16) : "";
     geoloactionDataToUpdate.push(modifiedHost);
   }
   return geoloactionDataToUpdate;
 };
 
+const updateInventoryData = async (inventoryData) => {
+  for (const inventory in inventoryData) {
+    await updateHostInventory(inventoryData[inventory]);
+  }
+}
+
 const callingAsyncAPIs = async () => {
-  const inventoryData = await getDataFromZabbix();
-  const filteredData = filterData(inventoryData.result);
-  console.log("FilteredData: \n ");
-  console.log(filteredData);
-  const hostDataToUpdate = await modifyHostData(filteredData);
-  console.log("hostDataToUpdate: \n ");
-  console.log(hostDataToUpdate);
+  const inventoryData = await getInventoryData();
+  const filteredInventoryData = filteInventoryrData(inventoryData.result);
+  const modifiedInventoryData = await modifyInventoryData(filteredInventoryData);
+  await updateInventoryData(modifiedInventoryData);
+  
 };
 
 callingAsyncAPIs();
